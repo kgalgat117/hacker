4e3cbae549f5d65ec34c2fb4497f17d124462de5
Karan <karangarg525@gmail.com>
2019-07-13 22:01:02 +0530

    garg_last_step

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import { FacebookComponent } from './facebook/facebook.component';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),
+    FacebookComponent],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
